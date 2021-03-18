d43cd1cbc173e3fe472a9c3917daaca62dc3daa1
BuildTools <unconfigured@null.spigotmc.org>
2021-03-18 22:12:47 +0000

    Update BowAimbot.h

--- Horion/Module/Modules/BowAimbot.h
+++ Horion/Module/Modules/BowAimbot.h
@@ -10,7 +10,7 @@ private:
 public:
 	BowAimbot();
 	~BowAimbot();
-
+	// Bow Aim Bot
 	virtual const char* getModuleName() override;
 	virtual void onPostRender(C_MinecraftUIRenderContext* renderCtx) override;
 	virtual void onSendPacket(C_Packet* packet) override;
